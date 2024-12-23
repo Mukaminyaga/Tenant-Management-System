2fd645129c997110a475ada2858a60dc0ccf439e
OgaoNeema <neemaogao@gmail.com>
2024-12-23 20:50:50 +0300

    Added navigation keys and content to the landing page

--- src/components/HomePageComponents/nav.jsx
+++ src/components/HomePageComponents/nav.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { signOutUser } from '../../redux/ActionCreators/authActionsCreator';
+import "./nav.css";
 
 const NavigationComponent = () => {
 
   const {isAuthenticated , user} = useSelector(state => state.auth);
   const dispatch = useDispatch();
 
-  return <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+  return <nav className='navbar navbar-expand-lg'>
     <Link className='navTitle ms-5' to='/'>TENANT HUB</Link>
 
     <ul className='navbar-nav ms-auto me-5'>
@@ -16,23 +17,23 @@ const NavigationComponent = () => {
         <>
         <li className='navItem'>
           <p className='my-0 mt-1 mx-2'>
-          <span className='text-light'>Welcome, </span> 
-          <span className='text-warning'>{user.displayName}</span> 
+          <span>Welcome, </span> 
+          <span className='username'>{user.displayName}</span> 
           </p>
         </li>
         <li className='navItem'>
           <Link className='btn btn-primary btn-sm mx-2' to='/Dashboard'>Dashboard</Link>
         </li>
         <li>
-          <button className='btn btn-success btn-sm' onClick={() => dispatch(signOutUser())}>Logout</button>
+          <button className='btn btn-secondary btn-sm' onClick={() => dispatch(signOutUser())}>Logout</button>
         </li>
         </>
       ) : (<>
             <li className='navItem'>
             <Link className='btn btn-primary btn-sm mx-2' to='/login'>Login</Link>
           </li>
-          <li>
-            <Link className='btn btn-success btn-sm' to='/signup'>Sign Up</Link>
+          <li className='navItem'>
+            <Link className='btn btn-primary btn-sm' to='/signup'>Sign Up</Link>
           </li>
           </>)
       }
